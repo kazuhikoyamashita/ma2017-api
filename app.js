c366c5201c1fd8142e3423af8646708ec2e6a5dc
Kazuhiko Yamashita <kazuhiko.yamashita@gmail.com>
2017-11-13 22:44:02 +0900

    感情と温泉の接続

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+
 var corser = require("corser");
 
 var index = require('./routes/index');
@@ -13,9 +14,11 @@ var spas = require('./routes/spas');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
